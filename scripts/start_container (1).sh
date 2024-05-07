5d0086db534246a1f9cfaa685c1de8c7a89915f2
Tns000 <41901400+Tns000@users.noreply.github.com>
2024-05-06 23:54:14 -0500

    Delete scripts/start_container (1).sh

--- scripts/start_container (1).sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker push isandyt/simple-python
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 isandyt/simple-python
