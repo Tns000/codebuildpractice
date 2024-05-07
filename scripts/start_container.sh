9a16567af7c1eb4b17986d25a605bd5051783880
Tns000 <41901400+Tns000@users.noreply.github.com>
2024-05-06 23:03:03 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push isandyt/simple-python:tagname
+docker push isandyt/simple-python
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 isandyt/simple-python:tagname
+docker run -d -p 5000:5000 isandyt/simple-python
